e71f220da117089582ef0d8adfa1951417894b61
brenandre23 <88087672+brenandre23@users.noreply.github.com>
2026-09-12 20:25:12 +0200

    Use final Design Studio deployment path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/regional-power-explorer/',
+  base: '/maps/2026/regional-power-explorer/',
   plugins: [react()],
 })
